8d12f7adabc7f8692ac709971e8158c692f0960b
Aarjap <piyaaarjap@gmail.com>
2021-01-12 16:16:39 -0600

    first

---
+++ src/tictactoe/TicTacToe.java
@@ -0,0 +1,25 @@
+
+package tictactoe;
+
+
+public class TicTacToe {
+
+  
+    public static void main(String[] args) {
+       char [][] gameboard = {{' ', '|', ' ', '|', ' '},
+                              {'-', '+', '-', '+', '-'},
+                              {' ', '|', ' ', '|', ' '},
+                             {'-', '+', '-', '+', '-'},
+                              {' ', '|', ' ', '|', ' '}};
+       
+             for (char[] row : gameboard){
+                 for(char c: row) {
+                     System.out.print(c);
+                 }
+                 System.out.println();
+                 
+             }                
+                                               
+       }
+    }
+    
